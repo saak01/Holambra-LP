010b75091feedcb24508778e16dce8004676131c
saak01 <joaovictorcosta.dev@gmail.com>
2024-01-29 21:23:48 -0300

    Responsividade header e footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,23 @@ import { HomeComponent } from './pages/home/home.component';
 import { FooterComponent } from './pages/footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatDividerModule} from '@angular/material/divider';
-
+import { HeaderComponent } from './pages/header/header.component';
+import {MatIconModule} from '@angular/material/icon';
+import { ChallengeComponent } from './pages/challenge/challenge.component';
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    FooterComponent
+    FooterComponent,
+    HeaderComponent,
+    ChallengeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatDividerModule
+    MatDividerModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
